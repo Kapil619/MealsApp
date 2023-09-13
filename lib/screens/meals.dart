2e9e3e7d6222353be6f19102a154be5664d98e7c
kapil619 <kapilbadokar321@gmail.com>
2023-09-13 11:49:52 +0530

    Adding MealDetails screen to mealitem widget

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 import '../models/meal.dart';
+import 'meal_details.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +50,8 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+        itemBuilder: (ctx, index) => MealItem(
+            meal: meals[index], onSelectMeal: selectMeal), //different approach
       );
     }
     return Scaffold(
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -7,8 +7,10 @@ class MealItem extends StatelessWidget {
   const MealItem({
     super.key,
     required this.meal,
+    required this.onSelectMeal,
   });
   final Meal meal;
+  final void Function(BuildContext context, Meal meal) onSelectMeal;
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
         meal.complexity.name.substring(1);
@@ -27,7 +29,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(context, meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
